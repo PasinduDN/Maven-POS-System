c322dc47ae8f2785fc337bcb3dc98261692c235f
Pasindu Dulanjana <pasindusl07@gmail.com>
2023-12-24 21:25:08 +0530

    Setting the customer window to open when the button is clicked on the dashboard

--- src/main/java/controller/DashboardFormController.java
+++ src/main/java/controller/DashboardFormController.java
@@ -1,11 +1,25 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DashboardFormController {
     public Button btnCustomer;
+    public AnchorPane pane;
 
     public void customerBtnOnAction(ActionEvent actionEvent) {
+        Stage stage = (Stage) pane.getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/CustomerForm.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
