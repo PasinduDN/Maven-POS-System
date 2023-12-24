cc8a515c96857b8dae93ac8f86fa95ae7fce23b6
Pasindu Dulanjana <pasindusl07@gmail.com>
2023-12-24 20:57:18 +0530

    Create DashboardForm.fxml and DashboardFormController.java

--- src/main/java/controller/DashboardFormController.java
+++ src/main/java/controller/DashboardFormController.java
@@ -1,4 +1,11 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class DashboardFormController {
+    public Button btnCustomer;
+
+    public void customerBtnOnAction(ActionEvent actionEvent) {
+    }
 }
