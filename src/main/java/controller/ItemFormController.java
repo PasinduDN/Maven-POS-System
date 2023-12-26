3dde146feea74240fcacbc8365a4a8a00b4a58ed
Pasindu Dulanjana <pasindusl07@gmail.com>
2023-12-26 10:54:27 +0530

    Implement Funtion For BackButton in ItemFormController.java

--- src/main/java/controller/ItemFormController.java
+++ src/main/java/controller/ItemFormController.java
@@ -2,10 +2,15 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ItemFormController {
 
@@ -50,7 +55,13 @@ public class ItemFormController {
 
     @FXML
     void backButtonOnAction(ActionEvent event) {
-
+        Stage stage = (Stage) tblItem.getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/DashboardForm.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
