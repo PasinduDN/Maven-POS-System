77bad0924c6f7131205949ea7fa41e2caaf1818d
Pasindu Dulanjana <pasindusl07@gmail.com>
2023-12-26 10:52:07 +0530

    Implement ItemFormController.java

--- src/main/java/controller/ItemFormController.java
+++ src/main/java/controller/ItemFormController.java
@@ -1,5 +1,63 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class ItemFormController {
 
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private TableColumn<?, ?> colCode;
+
+    @FXML
+    private TableColumn<?, ?> colDes;
+
+    @FXML
+    private TableColumn<?, ?> colOption;
+
+    @FXML
+    private TableColumn<?, ?> colPrice;
+
+    @FXML
+    private TableColumn<?, ?> colQty;
+
+    @FXML
+    private TableView<?> tblItem;
+
+    @FXML
+    private TextField txtCode;
+
+    @FXML
+    private TextField txtDes;
+
+    @FXML
+    private TextField txtPrice;
+
+    @FXML
+    private TextField txtQty;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    void backButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveBtnOnAction(ActionEvent event) {
+
+    }
+
+    public void updateBtnOnAction(ActionEvent actionEvent) {
+    }
 }
