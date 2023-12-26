a6c5159a5d622e106e5c6ad8302c51edb01f1462
Pasindu Dulanjana <pasindusl07@gmail.com>
2023-12-26 10:21:14 +0530

    Create function for Backbutton

--- src/main/java/controller/CustomerFormController.java
+++ src/main/java/controller/CustomerFormController.java
@@ -4,12 +4,16 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import model.Customer;
 import model.tm.CustomerTm;
 
+import java.io.IOException;
 import java.sql.*;
 
 public class CustomerFormController {
@@ -61,6 +65,13 @@ public class CustomerFormController {
 
     @FXML
     void backBtnOnAction(ActionEvent event) {
+        Stage stage = (Stage) tblCustomer.getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/DashboardForm.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
