3679c5eea953d022b69dad2c0e74a38c33a82cdd
Pasindu Dulanjana <pasindusl07@gmail.com>
2023-12-26 13:07:28 +0530

    Implement Save Button Action in  ItemFormController

--- src/main/java/controller/ItemFormController.java
+++ src/main/java/controller/ItemFormController.java
@@ -1,17 +1,21 @@
 package controller;
 
+import db.DBConnection;
+import dto.CustomerDto;
+import dto.ItemDto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import dto.tm.ItemTm;
 
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.sql.Statement;
 
 public class ItemFormController {
 
@@ -68,7 +72,33 @@ public class ItemFormController {
 
     @FXML
     void saveBtnOnAction(ActionEvent event) {
+        ItemDto dto = new ItemDto(
+                txtCode.getText(),
+                txtDes.getText(),
+                Double.parseDouble(txtPrice.getText()),
+                Integer.parseInt(txtQty.getText())
+                );
 
+        String sql = "INSERT INTO item VALUES ( ?,?,?,?)";
+
+        try {
+            PreparedStatement pstm = DBConnection.getInstance().getConnection().prepareStatement(sql);
+            pstm.setString(1,dto.getCode());
+            pstm.setString(2,dto.getDesc());
+            pstm.setDouble(3,dto.getUnitPrice());
+            pstm.setInt(4,dto.getQty());
+
+            int result = pstm.executeUpdate();
+            if (result>0){
+                new Alert(Alert.AlertType.INFORMATION,"Item Saved!").show();
+//                loadCustomerTable();
+//                clearfields();
+            }
+        }catch (SQLIntegrityConstraintViolationException ex){
+            new Alert(Alert.AlertType.ERROR,"Duplicate Entity").show();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void updateBtnOnAction(ActionEvent actionEvent) {
